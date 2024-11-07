aca04dccee5b5255ef0e17e64c1fc3f9c7144570
Nico-Sellitto <nico.sellitto@mths.ca>
2024-11-07 11:22:49 -0500

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nico Sellitto
+ * Created on: Nov 2024
+ * This program get the distance and if something is too close, brodcast "Too close!"
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(50)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+let distance = 0
+radio.setTransmitPower(7)
+
+// gets distance in cm
+while (true) {
+    basic.clearScreen()
+    distance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
+    // if distance is greator or equal to 10 then it sends a brodcast saying "Too close!"
+    if (distance <= 10) {
+        basic.clearScreen()
+        radio.sendString("Too close!")
+    }
+}
+
+// when a string is brodcasted, show it on the screen
+radio.onReceivedString(function(receivedString: string) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
